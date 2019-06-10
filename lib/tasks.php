6e8231660e29380ed49be8d8b6e444597082bedd
ion <vaneatka@gmail.com>
2019-06-10 16:56:51 +0300

    time+next, prev

--- lib/tasks.php
+++ lib/tasks.php
@@ -2,9 +2,16 @@
 function load_tasks($all=true ){
     $data = file_get_contents('database/tasks.json');
     $tasks = json_decode($data, true);
-    $t_page = ceil(count($tasks)/PER_PAGE);
+    $t_page = ceil(count($tasks)/PER_PAGE);    
     $c_page = $_GET['page'] ?? 1;
-    //pagination
+    
+    //pagination   
+    if($_GET['pag']=='prev'&& $c_page>1){
+        $c_page--;
+    };
+    if($_GET['pag']=='next'&& $c_page<$t_page){
+        $c_page++;
+    };
 if($all){
     $page = [
         'items' => $tasks,
@@ -17,7 +24,6 @@ if($all){
         'current_page' => $c_page,    
     ];
 }
-
     return $page;
 }
 
@@ -27,6 +33,13 @@ function save_task($task){
     $tasks[]=$task; 
     $data = json_encode($tasks, JSON_PRETTY_PRINT);
     file_put_contents('database/tasks.json', $data);
+}
+
+function date_difference($date){
+    $today = date_create('now');
+    $date = date_create($date);
+    $difference = date_diff($date, $today, true);
+    return $difference->format('%m months %d days ago');
 }
 
 ?>
--- templates/task-list.php
+++ templates/task-list.php
@@ -5,14 +5,13 @@
 
 <h3>Showing Tasks <?php print count($tasks['items'])?></h3>
 <?php foreach($tasks['items'] as $task){   ?>
-
     <li>
     <input type="checkbox" <?php print ($task['status']=="done")? "checked": null;  ?> >
     <a href="" style="text-decoration:none; color:black; cursor:default;">
     <?php print $task['title'] ?>    
     </a>
     <small>
-    <?php print $task['created']
+    <?php print date_difference($task['created'])
     // history date format,today, yesterday, this week, this year
     ?>
     </small>
@@ -22,7 +21,7 @@
 
 </ul>
 <hr>
-<a href=""><</a>
+<a href="?pag=prev"><</a>
 <?php for($p=1; $p<=$tasks['total_pages'];$p++){?>
 <a href="?page=<?php print $p?>"><?php 
 if($tasks['current_page']==$p){
@@ -34,7 +33,7 @@ if($tasks['current_page']==$p){
 ?></a>
 
 <?php } ?>
-<a href="">></a>
+<a href="?pag=next">></a>
 
 
 </div>
